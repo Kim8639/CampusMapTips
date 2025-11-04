a7bd64958d8b44a41605d0e93c2b8703d0d00ff4
Kim8639 <93768912+Kim8639@users.noreply.github.com>
2025-11-04 19:41:17 +0900

    Update Building.java

--- src/main/java/com/skhu/tips/model/dto/Building.java
+++ src/main/java/com/skhu/tips/model/dto/Building.java
@@ -9,6 +9,44 @@ package com.skhu.tips.model.dto;
  * ***************************
  */
 
-public class Building {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Building {
+    private int id;
+    private String name;
+    private String description;
+    private List<Facility> facilities;
+
+    public Building(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.facilities = new ArrayList<>();
+    }
+
+    public void addFacility(Facility facility) {
+        facilities.add(facility);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Facility> getFacilities() {
+        return facilities;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
