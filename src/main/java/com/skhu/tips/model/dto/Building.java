3444254104902861841088bcfde0d79ebfacb8ee
Kim8639 <93768912+Kim8639@users.noreply.github.com>
2025-11-04 23:58:53 +0900

    Update Building.java
    초안 1

--- src/main/java/com/skhu/tips/model/dto/Building.java
+++ src/main/java/com/skhu/tips/model/dto/Building.java
@@ -4,11 +4,49 @@ package com.skhu.tips.model.dto;
  * ***************************
  *  파일명: Building
  *  작성자: 김주환
- *  작성일:
- *  내용:
+ *  작성일:2025-11-04  
+ *  내용: 빌딩 dto
  * ***************************
  */
 
-public class Building {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Building {
+    private int id;
+    private String name;
+    private String description;
+    private List<Facility> facilities;
+
+    public Building(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.facilities = new ArrayList<>();
+    }
+
+    public void addFacility(Facility facility) {
+        facilities.add(facility);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Facility> getFacilities() {
+        return facilities;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
